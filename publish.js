ccbfa109abb1cf2ba5036443ad53338ebdd312d6
luxp <luxianpo@gmail.com>
2019-08-18 21:55:20 +0800

    update publish

--- publish.js
+++ publish.js
@@ -1,7 +1,7 @@
 const execSync = require('child_process').execSync
 
 execSync(
-  'git checkout master && git merge draft && git push && git checkout draft && node ./new-weekly.js',
+  'git checkout master && git merge draft && git push && git checkout draft && node ./new-weekly.js && git commit -am "init weekly" && git push',
   {
     stdio: 'inherit'
   }
